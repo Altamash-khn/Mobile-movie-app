63e3f1c8ae9ea7df8f492c64ce24480009ede5a0
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-01-19 21:32:52 +0530

    added tabs and custom tailwind classes

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Saved = () => {
+  return <Text>Saved Screen</Text>;
+};
+
+export default Saved;
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Search = () => {
+  return (
+    <Text className="mt-5 ml-3 text-2xl text-green-500">Search Screen</Text>
+  );
+};
+
+export default Search;
