c27c0f376d0bf86017408c9da130d6414a2c44b2
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-01-26 15:16:59 +0530

    feat: add dummy profile and saved screens

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,16 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
-  return <Text>Profile Screen</Text>;
+  return (
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex flex-1 flex-col justify-center items-center gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
+    </View>
+  );
 };
 
 export default Profile;
