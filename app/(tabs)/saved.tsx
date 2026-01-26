dcc9dce6435d39918c66bdb44147c35f6fb23194
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-01-26 15:19:34 +0530

    fix: add missing return statement

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Image, Text, View } from "react-native";
 
 const Saved = () => {
-  <View className="bg-primary flex-1 px-10">
-    <View className="flex flex-1 flex-col justify-center items-center gap-5">
-      <Image source={icons.save} className="size-10" tintColor="#fff" />
-      <Text className="text-gray-500 text-base">Saved</Text>
+  return (
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex flex-1 flex-col justify-center items-center gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Saved</Text>
+      </View>
     </View>
-  </View>;
+  );
 };
 
 export default Saved;
