e4a1703da01b44afb9a580c42f8e99469d17774a
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-01-31 00:00:55 +0530

    feat: add login functionality and validation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,7 +1,10 @@
+import { useAuth } from "@/context/AuthContext";
 import { router } from "expo-router";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 import {
   Pressable,
+  ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
@@ -10,51 +13,113 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 
 function Login() {
+  const { login } = useAuth();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function handleLogin(data: AuthFormData) {
+    try {
+      await login(data);
+      router.replace("/(tabs)");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <SafeAreaView className="flex-1 bg-[#0B0F1A]">
-      <View className="flex-1 px-6 justify-center">
-        <Text className="text-3xl font-bold text-white mb-2">
-          Welcome back 👋
-        </Text>
-        <Text className="text-gray-400 mb-8">
-          Login to continue watching movies
-        </Text>
-
-        <Text className="text-gray-300 mb-2">Email</Text>
-        <TextInput
-          placeholder="Enter your email"
-          placeholderTextColor="#6B7280"
-          className="bg-[#12182B] text-white rounded-xl px-4 py-4 mb-4"
-        />
-
-        <Text className="text-gray-300 mb-2">Password</Text>
-        <TextInput
-          placeholder="Enter your password"
-          placeholderTextColor="#6B7280"
-          secureTextEntry
-          className="bg-[#12182B] text-white rounded-xl px-4 py-4 mb-6"
-        />
-
-        <TouchableOpacity className="bg-violet-500 py-4 rounded-2xl mb-4">
-          <Text className="text-center text-white font-semibold text-lg">
-            Login
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="flex-1 px-6 justify-center">
+          <Text className="text-3xl font-bold text-white mb-2">
+            Welcome back 👋
           </Text>
-        </TouchableOpacity>
-
-        <Pressable onPress={() => router.replace("/(tabs)")} className="mb-6">
-          <Text className="text-center text-gray-500 underline">
-            Skip for now, go to Home
+          <Text className="text-gray-400 mb-8">
+            Login to continue watching movies
           </Text>
-        </Pressable>
 
-        <View className="flex-row justify-center">
-          <Text className="text-gray-400">Don't have an account? </Text>
+          <Text className="text-gray-300 mb-2 mt-4">Email</Text>
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: "email is required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Enter a valid email",
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                onChangeText={onChange}
+                placeholder="enter your email"
+                placeholderTextColor="#6B7280"
+                className="bg-[#12182B] text-white rounded-xl px-4 py-4 mb-3"
+              />
+            )}
+          />
+          {errors.email && (
+            <Text className="text-red-400">{errors.email.message}</Text>
+          )}
 
-          <Pressable onPress={() => router.replace("/signup")}>
-            <Text className="text-violet-400 font-semibold">Sign up</Text>
+          <Text className="text-gray-300 mb-2 mt-5">Password</Text>
+          <Controller
+            control={control}
+            name="password"
+            rules={{
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                onChangeText={onChange}
+                placeholder="enter your pasword"
+                secureTextEntry
+                placeholderTextColor="#6B7280"
+                className="bg-[#12182B] text-white rounded-xl px-4 py-4 mb-3"
+              />
+            )}
+          />
+          {errors.password && (
+            <Text className="text-red-400">{errors.password.message}</Text>
+          )}
+
+          <TouchableOpacity
+            className="bg-violet-500 py-4 rounded-2xl my-6"
+            onPress={handleSubmit(handleLogin)}
+          >
+            <Text className="text-center text-white font-semibold text-lg">
+              Login
+            </Text>
+          </TouchableOpacity>
+
+          <Pressable onPress={() => router.replace("/(tabs)")} className="mb-6">
+            <Text className="text-center text-gray-500 underline">
+              Skip for now, go to Home
+            </Text>
           </Pressable>
+
+          <View className="flex-row justify-center">
+            <Text className="text-gray-400">Don't have an account? </Text>
+
+            <Pressable onPress={() => router.replace("/signup")}>
+              <Text className="text-violet-400 font-semibold">Sign up</Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -3,6 +3,7 @@ import { router } from "expo-router";
 import { Controller, useForm } from "react-hook-form";
 import {
   Pressable,
+  ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
@@ -36,112 +37,116 @@ function Signup() {
 
   return (
     <SafeAreaView className="flex-1 bg-[#0B0F1A]">
-      <View className="flex-1 px-6 justify-center">
-        <Text className="text-3xl font-bold text-white mb-2">
-          Create account ✨
-        </Text>
-        <Text className="text-gray-400 mb-8">Join us and explore movies</Text>
-
-        <Text className="text-gray-300 mb-2">Name</Text>
-        <Controller // bridge between input values and react-hook-form
-          control={control} // tells react-hook-form to start tracking it
-          name="name" // must match with the one you wanna cnct in default values
-          rules={{
-            // validations
-            required: "Name is required",
-            minLength: {
-              value: 2,
-              message: "Name must be at least 2 characters",
-            },
-          }}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              value={value}
-              onChangeText={onChange}
-              placeholder="Enter your name"
-              placeholderTextColor="#6B7280"
-              className="bg-[#12182B] text-white rounded-xl px-4 py-4"
-            />
-          )}
-        />
-        {errors.name && (
-          <Text className="text-red-400 mt-1.5">{errors.name.message}</Text>
-        )}
-
-        <Text className="text-gray-300 mb-2 mt-4">Email</Text>
-        <Controller
-          control={control}
-          name="email"
-          rules={{
-            required: "Email is required",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "Enter a valid email",
-            },
-          }}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              value={value}
-              onChangeText={onChange}
-              placeholder="Enter your email"
-              placeholderTextColor="#6B7280"
-              className="bg-[#12182B] text-white rounded-xl px-4 py-4"
-            />
-          )}
-        />
-        {errors.email && (
-          <Text className="text-red-400 mt-1.5">{errors.email.message}</Text>
-        )}
-
-        <Text className="text-gray-300 mb-2 mt-4">Password</Text>
-        <Controller
-          control={control}
-          name="password"
-          rules={{
-            required: "Password is required",
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters",
-            },
-          }}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              value={value}
-              onChangeText={onChange}
-              placeholder="Create a password"
-              placeholderTextColor="#6B7280"
-              secureTextEntry
-              className="bg-[#12182B] text-white rounded-xl px-4 py-4 "
-            />
-          )}
-        />
-        {errors.password && (
-          <Text className="text-red-400 mt-1.5">{errors.password.message}</Text>
-        )}
-
-        <TouchableOpacity
-          className="bg-violet-500 py-4 rounded-2xl my-4"
-          onPress={handleSubmit(handleSignup)}
-        >
-          <Text className="text-center text-white font-semibold text-lg">
-            Sign Up
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="flex-1 px-6 justify-center">
+          <Text className="text-3xl font-bold text-white mb-2">
+            Create account ✨
           </Text>
-        </TouchableOpacity>
+          <Text className="text-gray-400 mb-8">Join us and explore movies</Text>
 
-        <Pressable onPress={() => router.replace("/(tabs)")} className="mb-6">
-          <Text className="text-center text-gray-500 underline">
-            Skip for now, go to Home
-          </Text>
-        </Pressable>
+          <Text className="text-gray-300 mb-2">Name</Text>
+          <Controller // bridge between input values and react-hook-form
+            control={control} // tells react-hook-form to start tracking it
+            name="name" // must match with the one you wanna cnct in default values
+            rules={{
+              // validations
+              required: "Name is required",
+              minLength: {
+                value: 2,
+                message: "Name must be at least 2 characters",
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                onChangeText={onChange}
+                placeholder="Enter your name"
+                placeholderTextColor="#6B7280"
+                className="bg-[#12182B] text-white rounded-xl px-4 py-4"
+              />
+            )}
+          />
+          {errors.name && (
+            <Text className="text-red-400 mt-1.5">{errors.name.message}</Text>
+          )}
 
-        <View className="flex-row justify-center">
-          <Text className="text-gray-400">Already have an account? </Text>
+          <Text className="text-gray-300 mb-2 mt-4">Email</Text>
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: "Email is required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Enter a valid email",
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                onChangeText={onChange}
+                placeholder="Enter your email"
+                placeholderTextColor="#6B7280"
+                className="bg-[#12182B] text-white rounded-xl px-4 py-4"
+              />
+            )}
+          />
+          {errors.email && (
+            <Text className="text-red-400 mt-1.5">{errors.email.message}</Text>
+          )}
 
-          <Pressable onPress={() => router.replace("/login")}>
-            <Text className="text-violet-400 font-semibold">login</Text>
+          <Text className="text-gray-300 mb-2 mt-4">Password</Text>
+          <Controller
+            control={control}
+            name="password"
+            rules={{
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                onChangeText={onChange}
+                placeholder="Create a password"
+                placeholderTextColor="#6B7280"
+                secureTextEntry
+                className="bg-[#12182B] text-white rounded-xl px-4 py-4 "
+              />
+            )}
+          />
+          {errors.password && (
+            <Text className="text-red-400 mt-1.5">
+              {errors.password.message}
+            </Text>
+          )}
+
+          <TouchableOpacity
+            className="bg-violet-500 py-4 rounded-2xl my-4"
+            onPress={handleSubmit(handleSignup)}
+          >
+            <Text className="text-center text-white font-semibold text-lg">
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+
+          <Pressable onPress={() => router.replace("/(tabs)")} className="mb-6">
+            <Text className="text-center text-gray-500 underline">
+              Skip for now, go to Home
+            </Text>
           </Pressable>
+
+          <View className="flex-row justify-center">
+            <Text className="text-gray-400">Already have an account? </Text>
+
+            <Pressable onPress={() => router.replace("/login")}>
+              <Text className="text-violet-400 font-semibold">login</Text>
+            </Pressable>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
